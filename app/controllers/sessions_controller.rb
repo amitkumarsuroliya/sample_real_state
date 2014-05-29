9a3fb75f4dee21c65a9b588ba46622022558eab5
Arvind Mehra <your.bloomberg490@gmail.com>
2014-05-29 22:23:06 +0530

    heroku fix change admin to devise

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-class Admins::SessionsController < Devise::SessionsController
+class Devise::SessionsController < Devise::SessionsController
 
 	prepend_before_filter :require_no_authentication, :only => [ :new, :create ]
   prepend_before_filter :allow_params_authentication!, :only => :create
